0f7e53938f277b25116dbca30cd59df021bb9c23
SeunghwanKang <46666296+kangshwan@users.noreply.github.com>
2024-03-08 11:18:02 +0900

    Feature/23 change measurement method t6003 (#24)
    * Feat: change measurement method
    추론 시간 측정 방법 변경
    1_img 1000 time -> all testset 10 time
    issue: #23
    
    * Fix: change repetitions
    182 -> 1820으로 변경
    issue: #23
    
    * Update: change measurement method
    issue: #23
    
    ---------
    
    Co-authored-by: seonghyeokcho <whtjdgur77@gmail.com>

--- PET/measure_inference_time.py
+++ PET/measure_inference_time.py
@@ -1,77 +1,101 @@
-import argparse
-import random
-from pathlib import Path
+import re 
 import os
+import glob
+import random
+import argparse
 
+import wandb
+import cv2
 import numpy as np
 import torch
-import torch.nn as nn
-from torch.utils.data import DataLoader, DistributedSampler
-
-import datasets
-from datasets import build_dataset
 import util.misc as utils
-from engine import evaluate
-from models import build_model
-
-import cv2
-import torchvision.transforms as standard_transforms
 from PIL import Image
 from tqdm import tqdm
+import torchvision.transforms as standard_transforms
+
+from models import build_model
+
+
+
+
+def natural_keys(text):
+    """
+    alist.sort(key=natural_keys)를 사용하면, 텍스트에 포함된 숫자를 기준으로 정렬할 수 있습니다.
+    """
+    return [int(c) if c.isdigit() else c for c in re.split("(\d+)", text)]
+
 
 def get_args_parser():
-    parser = argparse.ArgumentParser('Set Point Query Transformer', add_help=False)
+    parser = argparse.ArgumentParser("Set Point Query Transformer", add_help=False)
 
     # model parameters
     # - backbone
-    parser.add_argument('--backbone', default='vgg16_bn', type=str,
+    parser.add_argument("--backbone", default="vgg16_bn", type=str,
                         help="Name of the convolutional backbone to use")
-    parser.add_argument('--position_embedding', default='sine', type=str, choices=('sine', 'learned', 'fourier'),
+    parser.add_argument("--position_embedding", default="sine", type=str, choices=("sine", "learned", "fourier"),
                         help="Type of positional embedding to use on top of the image features")
     # - transformer
-    parser.add_argument('--dec_layers', default=2, type=int,
+    parser.add_argument("--dec_layers", default=2, type=int,
                         help="Number of decoding layers in the transformer")
-    parser.add_argument('--dim_feedforward', default=512, type=int,
+    parser.add_argument("--dim_feedforward", default=512, type=int,
                         help="Intermediate size of the feedforward layers in the transformer blocks")
-    parser.add_argument('--hidden_dim', default=256, type=int,
+    parser.add_argument("--hidden_dim", default=256, type=int,
                         help="Size of the embeddings (dimension of the transformer)")
-    parser.add_argument('--dropout', default=0.0, type=float,
+    parser.add_argument("--dropout", default=0.0, type=float,
                         help="Dropout applied in the transformer")
-    parser.add_argument('--nheads', default=8, type=int,
+    parser.add_argument("--nheads", default=8, type=int,
                         help="Number of attention heads inside the transformer's attentions")
     
     # loss parameters
     # - matcher
-    parser.add_argument('--set_cost_class', default=1, type=float,
+    parser.add_argument("--set_cost_class", default=1, type=float,
                         help="Class coefficient in the matching cost")
-    parser.add_argument('--set_cost_point', default=0.05, type=float,
+    parser.add_argument("--set_cost_point", default=0.05, type=float,
                         help="SmoothL1 point coefficient in the matching cost")
     # - loss coefficients
-    parser.add_argument('--ce_loss_coef', default=1.0, type=float)       # classification loss coefficient
-    parser.add_argument('--point_loss_coef', default=5.0, type=float)    # regression loss coefficient
-    parser.add_argument('--eos_coef', default=0.5, type=float,
+    parser.add_argument("--ce_loss_coef", default=1.0, type=float)       # classification loss coefficient
+    parser.add_argument("--point_loss_coef", default=5.0, type=float)    # regression loss coefficient
+    parser.add_argument("--eos_coef", default=0.5, type=float,
                         help="Relative classification weight of the no-object class")   # cross-entropy weights
 
     # dataset parameters
-    parser.add_argument('--dataset_file', default="SHA")
-    parser.add_argument('--data_path', default="./data/ShanghaiTech/PartA", type=str)
+    parser.add_argument("--dataset_file", default="SHA")
+    parser.add_argument("--data_path", default="./data/ShanghaiTech/PartA", type=str)
 
     # misc parameters
-    parser.add_argument('--device', default='cuda',
-                        help='device to use for training / testing')
-    parser.add_argument('--seed', default=42, type=int)
-    parser.add_argument('--resume', default='', help='resume from checkpoint')
-    parser.add_argument('--vis_dir', default="")
-    parser.add_argument('--num_workers', default=2, type=int)
+    parser.add_argument("--device", default="cuda",
+                        help="device to use for training / testing")
+    parser.add_argument("--seed", default=42, type=int)
+    parser.add_argument("--resume", default="", help="resume from checkpoint")
+    parser.add_argument("--vis_dir", default="")
+    parser.add_argument("--num_workers", default=2, type=int)
 
     # distributed training parameters
-    parser.add_argument('--world_size', default=1, type=int,
-                        help='number of distributed processes')
-    parser.add_argument('--dist_url', default='env://', help='url used to set up distributed training')
+    parser.add_argument("--world_size", default=1, type=int,
+                        help="number of distributed processes")
+    parser.add_argument("--dist_url", default="env://", help="url used to set up distributed training")
+    
+    # measure mode
+    parser.add_argument("--repetitions", type=int, default=10)
+    
+    # transformer
+    parser.add_argument("--transformer_method", default="basic", type=str)
+    
+    # wandb
+    parser.add_argument("--use_wandb", type=int, default=0)
+    parser.add_argument("--wandb_name", type=str, default="default")
     return parser
 
 
 def main(args):
+    # logging with wandb
+    if args.use_wandb:
+        wandb.init(
+            entity="level2_cv4_dc",
+            project="final-nota-inference",
+            name=args.wandb_name,
+            config=args)
+    
     utils.init_distributed_mode(args)
     print(args)
     device = torch.device(args.device)
@@ -85,61 +109,75 @@ def main(args):
     # build model
     model, criterion = build_model(args)
     model.to(device)
-
-    model_without_ddp = model
-    if args.distributed:
-        model = torch.nn.parallel.DistributedDataParallel(model, device_ids=[args.gpu], find_unused_parameters=True)
-        model_without_ddp = model.module
+    
     n_parameters = sum(p.numel() for p in model.parameters() if p.requires_grad)
-    print('params:', n_parameters/1e6)
+    print("params:", n_parameters / 1e6)
 
     # load pretrained model
-    if args.resume:
-        if args.resume.startswith('https'):
-            checkpoint = torch.hub.load_state_dict_from_url(
-                args.resume, map_location='cpu', check_hash=True)
-        else:
-            checkpoint = torch.load(args.resume, map_location='cpu')
-        model_without_ddp.load_state_dict(checkpoint['model'])
-        cur_epoch = checkpoint['epoch'] if 'epoch' in checkpoint else 0
+    checkpoint = torch.load(args.resume, map_location="cpu")
+    model.load_state_dict(checkpoint["model"])
+
+    # get image paths
+    img_dir = "./data/ShanghaiTech/part_A/test_data/images"
+    img_paths = glob.glob(os.path.join(img_dir, "*.jpg"))
+    num_images = len(img_paths)
+
+    # natural_keys 함수를 사용하여 정렬
+    img_paths.sort(key=natural_keys)
     
-    # read image
-    img_path = './IMG_57.jpg'
-    img = cv2.imread(img_path)
+    transform = standard_transforms.Compose(
+        [
+            standard_transforms.ToTensor(),
+            standard_transforms.Normalize(
+                mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]
+            ),
+        ]
+    )
+
+    repetitions = args.repetitions
+    timings = np.zeros((num_images, repetitions))
+    
+    img = cv2.imread("./IMG_57.jpg")
     img = Image.fromarray(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
-    
-    transform = standard_transforms.Compose([
-        standard_transforms.ToTensor(), standard_transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                                                                      std=[0.229, 0.224, 0.225]),
-    ])
-    
     img = transform(img)
     img = torch.Tensor(img).to(device)
     img.unsqueeze_(0)
-    print(img.shape)
-    
-    starter, ender = torch.cuda.Event(enable_timing=True), torch.cuda.Event(enable_timing=True)
-    repetitions = 1000
-    timings = np.zeros((repetitions, 1))
     for _ in range(10):
         _ = model(img, test=True)
 
-    with torch.no_grad():
-        for rep in tqdm(range(repetitions)):
-            starter.record()
-            test_stats = model(img, test=True)
-            ender.record()
-            torch.cuda.synchronize()
-            curr_time = starter.elapsed_time(ender)
-            timings[rep] = curr_time
-            
+    starter, ender = torch.cuda.Event(enable_timing=True), torch.cuda.Event(enable_timing=True)
+    
+    for i, img_path in enumerate(tqdm(img_paths)):
+        img = cv2.imread(img_path)
+        img = Image.fromarray(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+        img = transform(img)
+        img = torch.Tensor(img).to(device)
+        img.unsqueeze_(0)
+        
+        with torch.no_grad():
+            for j in range(repetitions):
+                starter.record()
+                _ = model(img, test=True)
+                ender.record()
+                torch.cuda.synchronize()
+                curr_time = starter.elapsed_time(ender)
+                timings[i, j] = curr_time
+        if args.use_wandb:
+            wandb.log({"Avg Inference Time per Image": np.mean(timings[i])})
 
-    mean_syn = np.sum(timings) / repetitions
-    std_syn = np.std(timings)
+    mean_time = np.mean(timings)
+    std_dev = np.std(timings)
+    
+    if args.use_wandb:
+        wandb.log({
+            "Avg Inference Time": mean_time,
+            "Std Dev": std_dev
+            })
+        
+    print(f"Mean time = {mean_time}ms, std = {std_dev}ms")
+    
 
-    print(f"Mean time = {mean_syn}ms, std = {std_syn}ms")
-
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser('PET evaluation script', parents=[get_args_parser()])
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser("PET evaluation script", parents=[get_args_parser()])
     args = parser.parse_args()
     main(args)
