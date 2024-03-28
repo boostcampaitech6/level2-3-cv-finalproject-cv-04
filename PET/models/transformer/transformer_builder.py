311ff9ed29367ad8a73578ce5ae8b19a802c0540
kangshwan <rnltls97@gmail.com>
2024-03-28 21:15:40 +0900

    Feat: 기존 PET 모델에서 Encoder layer 개수를 2개로 변경 기본적인 변경 및 swin transformer, pooling attention 변경 실험 진행 issue: #28

--- PET/models/transformer/transformer_builder.py
+++ PET/models/transformer/transformer_builder.py
@@ -1,7 +1,7 @@
 def build_encoder(args, **kwargs):
     # kwargs : enc_win_list = [(32, 16), (32, 16), (16, 8), (16, 8)]
     # d_model=256, dropout=0.0, nhead=8, dim_feedforward=512,num_encoder_layers=4
-    if args.swin:
+    if args.transformer_method == 'swin':
         from .prog_swin_transformer import WinEncoderTransformer
         return WinEncoderTransformer(
             d_model=args.hidden_dim,
@@ -12,7 +12,7 @@ def build_encoder(args, **kwargs):
             activation="gelu",  #### CHANGE
             **kwargs,
         )
-    elif args.swinpool:
+    elif args.transformer_method == 'swinpool':
         from .prog_swin_poolformer import WinEncoderTransformer
         return WinEncoderTransformer(
             d_model=args.hidden_dim,
@@ -21,6 +21,60 @@ def build_encoder(args, **kwargs):
             dim_feedforward=args.dim_feedforward,
             num_encoder_layers=args.enc_layers,
             activation="gelu",  #### CHANGE
+            **kwargs,
+        )
+    elif args.transformer_method == 'two_encoder':
+        from .prog_2_encoder_win_transformer import WinEncoderTransformer
+        kwargs['enc_win_list'] = [(32, 16), (16, 8)]
+        kwargs['transformer_method'] = args.transformer_method
+        return WinEncoderTransformer(
+            d_model=args.hidden_dim,
+            dropout=args.dropout,
+            nhead=args.nheads,
+            dim_feedforward=args.dim_feedforward,
+            num_encoder_layers=len(kwargs['enc_win_list']),
+            activation="gelu",  #### CHANGE
+            **kwargs,
+        )
+    elif args.transformer_method == 'two_encoder_pooling':
+        from .prog_2_encoder_win_transformer_pooling_NEW import WinEncoderTransformer
+        kwargs['enc_win_list'] = [(32, 16), (16, 8)]
+        kwargs['transformer_method'] = args.transformer_method
+        return WinEncoderTransformer(
+            d_model=args.hidden_dim,
+            dropout=args.dropout,
+            nhead=args.nheads,
+            dim_feedforward=args.dim_feedforward,
+            num_encoder_layers=len(kwargs['enc_win_list']),
+            activation="gelu",  #### CHANGE
+            **kwargs,
+        )
+    elif args.transformer_method == 'three_encoder':
+        from .prog_2_encoder_win_transformer import WinEncoderTransformer
+        kwargs['enc_win_list'] = [(32, 16), (32, 16), (16, 8)]
+        kwargs['transformer_method'] = args.transformer_method
+        return WinEncoderTransformer(
+            d_model=args.hidden_dim,
+            dropout=args.dropout,
+            nhead=args.nheads,
+            dim_feedforward=args.dim_feedforward,
+            num_encoder_layers=len(kwargs['enc_win_list']),
+            activation="gelu",  #### CHANGE
+            **kwargs,
+        )
+    elif args.transformer_method == 'swin_patch_merging_FPN64_2Encoder':
+        from .prog_2_encoder_swin_transformer import WinEncoderTransformer, PatchMerging
+        
+        kwargs['enc_win_list'] =[(32, 16), (32, 16)]
+
+        return WinEncoderTransformer(
+            d_model=args.hidden_dim,
+            dropout=args.dropout,
+            nhead=args.nheads,
+            dim_feedforward=args.dim_feedforward,
+            num_encoder_layers=len(kwargs['enc_win_list']),
+            activation="gelu",  #### CHANGE
+            downsample=PatchMerging,
             **kwargs,
         )
     else:
@@ -37,7 +91,7 @@ def build_encoder(args, **kwargs):
 
 
 def build_decoder(args, **kwargs):
-    if args.swin:
+    if args.transformer_method == 'swin':
         from .prog_swin_transformer import WinDecoderTransformer
         return WinDecoderTransformer(
             d_model=args.hidden_dim,
@@ -47,8 +101,38 @@ def build_decoder(args, **kwargs):
             num_decoder_layers=args.dec_layers,
             return_intermediate_dec=True,
         )
-    elif args.swinpool:
+    elif args.transformer_method == 'swinpool':
         from .prog_swin_poolformer import WinDecoderTransformer
+        return WinDecoderTransformer(
+            d_model=args.hidden_dim,
+            dropout=args.dropout,
+            nhead=args.nheads,
+            dim_feedforward=args.dim_feedforward,
+            num_decoder_layers=args.dec_layers,
+            return_intermediate_dec=True,
+        )
+    elif args.transformer_method == 'two_encoder':
+        from .prog_2_encoder_win_transformer import WinDecoderTransformer
+        return WinDecoderTransformer(
+            d_model=args.hidden_dim,
+            dropout=args.dropout,
+            nhead=args.nheads,
+            dim_feedforward=args.dim_feedforward,
+            num_decoder_layers=args.dec_layers,
+            return_intermediate_dec=True,
+        )
+    elif args.transformer_method == 'two_encoder_pooling':
+        from .prog_2_encoder_win_transformer_pooling_NEW import WinDecoderTransformer
+        return WinDecoderTransformer(
+            d_model=args.hidden_dim,
+            dropout=args.dropout,
+            nhead=args.nheads,
+            dim_feedforward=args.dim_feedforward,
+            num_decoder_layers=args.dec_layers,
+            return_intermediate_dec=True,
+        )
+    elif args.transformer_method == 'swin_patch_merging_FPN64_2Encoder':
+        from .prog_2_encoder_swin_transformer import WinDecoderTransformer
         return WinDecoderTransformer(
             d_model=args.hidden_dim,
             dropout=args.dropout,
@@ -65,5 +149,5 @@ def build_decoder(args, **kwargs):
             nhead=args.nheads,
             dim_feedforward=args.dim_feedforward,
             num_decoder_layers=args.dec_layers,
-            return_intermediate_dec=True,
+            # return_intermediate_dec=True,
         )
